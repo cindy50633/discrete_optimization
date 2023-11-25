47c238170fb2be38ce9fafadd3e9a47dbd317275
cindy50633 <cindy50633@gmail.com>
2023-11-25 18:13:12 +0900

    refactor: rename variable name and function name

--- set_cover/branch_and_bound_solve.py
+++ set_cover/branch_and_bound_solve.py
@@ -1,6 +1,6 @@
-def is_valid_solution(solution, item_to_included_set_dict):
+def is_valid_solution(solution, item_to_sets):
     """Check if the current solution satisfies all constraints."""
-    for item, sets in item_to_included_set_dict.items():
+    for item, sets in item_to_sets.items():
         if not any(solution[set.index] for set in sets):
             return False
     return True
@@ -14,16 +14,25 @@ def calc_total_cost(candidate_set_dict, solution):
     return total_cost
 
 
-def dummy_solve(
-        candidate_set_dict, item_to_included_set_dict,
-        solution=None, idx=0, best_solution=None):
+def cost_bound_solve(
+        candidate_set_dict, item_to_sets,
+        best_solution=None,
+        solution=None, idx=0):
+
+    if best_solution is None:
+        best_solution = [1] * len(candidate_set_dict)
     if solution is None:
         solution = [0] * len(candidate_set_dict)
-        best_solution = [1] * len(candidate_set_dict)
+
+    my_cost = calc_total_cost(candidate_set_dict, solution)
+    best_cost = calc_total_cost(candidate_set_dict, best_solution)
+
+    if my_cost > best_cost:
+        return
 
     # Base case: If all sets are considered
     if idx == len(candidate_set_dict):
-        if is_valid_solution(solution, item_to_included_set_dict):
+        if is_valid_solution(solution, item_to_sets):
             my_cost = calc_total_cost(candidate_set_dict, solution)
             best_cost = calc_total_cost(candidate_set_dict, best_solution)
             # in-place change used to make object value change across functions
@@ -34,20 +43,21 @@ def dummy_solve(
 
     # Try including the current set in the solution
     solution[idx] = 1
-    dummy_solve(
+    cost_bound_solve(
         candidate_set_dict,
-        item_to_included_set_dict,
+        item_to_sets,
+        best_solution,
         solution.copy(),
-        idx + 1,
-        best_solution)
+        idx + 1
+        )
 
     # Try excluding the current set from the solution and backtrack
     solution[idx] = 0
-    dummy_solve(
+    cost_bound_solve(
         candidate_set_dict,
-        item_to_included_set_dict,
+        item_to_sets,
+        best_solution,
         solution.copy(),
-        idx + 1,
-        best_solution)
+        idx + 1)
 
     return best_solution
